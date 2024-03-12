d4ca4992e79fa4ea28a7c19e2c0ca6f989932ad0
rej <rzioma@acm.org>
2024-03-12 16:44:23 +0100

    const_matrix() helper

--- test/utils.py
+++ test/utils.py
@@ -33,6 +33,11 @@ def random_matrix(lo, hi, dims):
 assert random_matrix(0, 0, 4) == [0, 0, 0, 0]
 assert random_matrix(0, 0, (2, 2)) == [[0, 0], [0, 0]]
 
+def const_matrix(val, dims):
+    return random_matrix(val, val, dims)
+assert const_matrix(1, 4) == [1, 1, 1, 1]
+assert const_matrix(3, (2,2)) == [[3, 3], [3, 3]]
+
 def shape(a):
     if isinstance(a, (int, float)):
         return ()
